babb81ed94680d62e525cd2fb0af00c25347c4be
Natalia Cristóbal <natalia.cristobal1@educa.madrid.org>
2020-11-25 20:47:05 +0100

    Estructura do-while creada

--- src/Practica4.java
+++ src/Practica4.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Practica4 {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+
+        int estructura;
+        do {
+            System.out.println("Introduce el ejercicio que quieras resolver:");
+            System.out.println("1-For");
+            System.out.println("2-While");
+            estructura = sc.nextInt();
+
+
+
+
+
+        } while (estructura != 1 && estructura != 2);
+
+
     }
 }
